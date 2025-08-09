8b8ecd03503b3fdc17c98512b0035fae3b098033
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2025-08-09 18:41:45 +0200

    Try skipping the source_* configs

--- docs/conf.py
+++ docs/conf.py
@@ -99,9 +99,9 @@ html_theme_options: Dict[str, Any] = {
             "class": "",
         },
     ],
-    "source_repository": "https://github.com/pradyunsg/furo/",
-    "source_branch": "main",
-    "source_directory": "docs/",
+    # "source_repository": "https://github.com/pradyunsg/furo/",
+    # "source_branch": "main",
+    # "source_directory": "docs/",
 }
 
 if "READTHEDOCS" in os.environ:
